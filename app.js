b8075da464d2a00d163acf4fca90d64680616e97
deepak <deepakb@protonmail.com>
2020-04-14 15:18:32 +0530

    added Block class

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const { SHA256 } = require('crypto-js');
 
-let difficulty = 3;
-let hash = "0";
-let nounce = 0;
-//136923 0000
-function hasher(){
-	let start = Date.now()
-	while (hash.substr(0, 3) != "123") {
-	    hash = SHA256("This is a secret key " + nounce).toString();
-	    nounce+=1;
-	    console.log(nounce + "   " + hash);
-	}
-	let end = Date.now()
-	console.log("hash complete at:"+ (end-start)/1000);
+class Block {
+    constructor(num, name, amount, prevHash = "0") {
+    	console.log("prev Hash:"+prevHash);
+    	this.blockNum = num;
+        this.name = name;
+        this.amount = amount;
+        this.nonce = 0;
+        this.timestamp = Date.now();
+        this.hash = "";
+        this.prevHash = prevHash;
+    }
+    mineBlock(difficulty) {
+        while (this.hash.substr(0, difficulty) != Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.hasher();
+            //console.log(this.hash);
+        }
+        return this;
+
+    }
+    hasher() {
+        //let start = Date.now()
+        let blockstring = `SHA256${this.name}${this.amount}${this.timestamp}${this.nonce}${this.prevHash}`;
+        return SHA256(blockstring).toString();
+
+        /*let end = Date.now()
+        console.log("hash complete at:"+ (end-start)/1000);*/
+    }
+
+
 }
-hasher();
+
+let block1 = new Block(1,"Genesis",100);
+block1.mineBlock(3);
+console.log(block1);
+console.log("Hash is: "+block1.hash);
+
+let block2 = new Block(2,"second block",200,block1.hash);
+block2.mineBlock(3);
+console.log(block2);
+console.log("Hash is: "+block2.hash);
