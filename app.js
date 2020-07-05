5f27c23263b9661077e7b9c8df660c89bb228e98
deepak <deepakb@protonmail.com>
2020-07-05 14:20:17 +0530

    Added download link for saving latest blockchain Inserted transactionList to io.sockets. Alert user when new blockchain received. Insert userTransaction input fields dynamically after login.

--- app.js
+++ app.js
@@ -17,12 +17,10 @@ const MASTER_KEY = KEYS.masterKey;
 const DIFFICULTY = 4;
 const DUMMY_DB = []; // for testing
 
-let transactionList = [];
 let auto_id = 0;
 let miningActive = false;
 
 
-// Socket Handling - Decentralize 
 
 io.on('connection', (socket) => {
     console.log('New socket connection', socket.id);
@@ -31,13 +29,20 @@ io.on('connection', (socket) => {
     }
     if (!io.sockets.connected_bchain_users) {
         io.sockets.connected_bchain_users = [];
+        io.sockets.transactionList = [];
     }
-        
-    socket.on("addToUserList", (user_id) => {
-        console.log("addToUserList:"+user_id);        
-        io.sockets.connected_bchain_users.push({user_id,sockedID: socket.id});
-        console.log(io.sockets.connected_bchain_users);
-        socket.emit("ConnectedUsers", io.sockets.connected_bchain_users.length);
+
+    socket.on("addToUserList", (user) => {
+        const tokenUser = tokenManager.readToken(user.token);
+        if (!tokenUser.error) {
+            if (!isDuplicateUser(tokenUser.userid)) {
+                console.log("addToUserList:" + tokenUser.userid);
+                io.sockets.connected_bchain_users.push({ user_id: tokenUser.userid, sockedID: socket.id });
+                socket.emit("ConnectedUsers", io.sockets.connected_bchain_users.length);
+                console.log(io.sockets.connected_bchain_users);
+            }
+        }
+
     });
 
     socket.on('blockChainConnect', (data) => {
@@ -59,8 +64,7 @@ io.on('connection', (socket) => {
     });
     socket.on('disconnect', () => {
         io.sockets.connected_bchain_users = io.sockets.connected_bchain_users.filter(conUser => conUser.sockedID !== socket.id);
-        console.log("disconnected:"+socket.id);
-        console.log(io.sockets.connected_bchain_users)
+        console.log("disconnected:" + socket.id);
     });
 
 });
@@ -71,11 +75,11 @@ io.on('connection', (socket) => {
 app.post("/register", (req, res) => {
     const { email, password } = req.body;
     const isEmailValid = (/(^[.a-z0-9_\-]{3,30})@[a-z]{3,15}\.(com|in|co.in|org|net)$/).test(email);
-    if(!isEmailValid){
-       return res.json({status:"Email Invalid"}) 
+    if (!isEmailValid) {
+        return res.json({ status: "Email Invalid" })
     }
     if (DUMMY_DB.some(user => user.email == email)) {
-        return res.json({status:"Email id is already registered."})
+        return res.json({ status: "Email id is already registered." })
     }
     const hashedPass = SHA256(password).toString();
     const userID = NEW_USER.getID();
@@ -90,43 +94,43 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
     const { userid, password } = req.body;
     if (!DUMMY_DB.some(user => user.id == userid)) {
-        return res.json({status:"Sorry, ID not registered"})
+        return res.json({ status: "Sorry, ID not registered" })
     }
     const hashedPass = SHA256(password).toString();
     if (!DUMMY_DB.some(user => (user.id == userid && user.pass == hashedPass))) {
-        return res.json({status:"Sorry, invalid ID/password combination"})
+        return res.json({ status: "Sorry, invalid ID/password combination" })
     }
-    if(isDuplicateUser(userid)){
-        return res.json({status:"multiple_login"})
+    if (isDuplicateUser(userid)) {
+        return res.json({ status: "multiple_login" })
     }
     const mytoken = tokenManager.createToken(userid);
-    return res.json({ status:"done", token: mytoken, tokenValidity: tokenManager.getExpiry() });
+    return res.json({ status: "done", token: mytoken, tokenValidity: tokenManager.getExpiry() });
 
 });
 
-app.post("/checktoken", (req, res) => {    
+app.post("/checktoken", (req, res) => {
     const tokenState = validateToken(req.body.token);
-    if(tokenState.includes("Error:")){
-        return res.json({status:tokenState})
-    }    
-   console.log("Connected Users:");
-   console.log(io.sockets.connected_bchain_users);
-   const tokenUser = tokenManager.readToken(req.body.token);
-   if(isDuplicateUser(tokenUser.userid)){
-        return res.json({status:"multiple_login"})
-   }   
-    return res.json({status:"valid",user:tokenUser.userid})    
+    if (tokenState.includes("Error:")) {
+        return res.json({ status: tokenState })
+    }
+    console.log("Connected Users:");
+    console.log(io.sockets.connected_bchain_users);
+    const tokenUser = tokenManager.readToken(req.body.token);
+    if (isDuplicateUser(tokenUser.userid)) {
+        return res.json({ status: "multiple_login" })
+    }
+    return res.json({ status: "valid", user: tokenUser.userid })
 
 });
 
-function isDuplicateUser(userid){
-    if(io.sockets.connected_bchain_users){
+function isDuplicateUser(userid) {
+    if (io.sockets.connected_bchain_users) {
         const existingUser = io.sockets.connected_bchain_users.filter(connected => connected.user_id == userid);
-        if(existingUser.length){
+        if (existingUser.length) {
             return true
         }
-   }
-   return false
+    }
+    return false
 }
 
 
@@ -141,7 +145,7 @@ function getBlockChain() {
 
 
 app.get("/transactions/", (req, res) => {
-    if (transactionList.length == 0) {
+    if (io.sockets.transactionList.length == 0) {
         return res.send("No pending transactions")
     }
 
@@ -151,7 +155,7 @@ app.get("/transactions/", (req, res) => {
 
 // List all pending transactions
 app.get("/transactions/:userid", (req, res) => {
-    if (transactionList.length == 0) {
+    if (io.sockets.transactionList.length == 0) {
         return res.send("No pending transactions")
     }
     getPendingTransactions(req.params.userid, res);
@@ -166,24 +170,25 @@ app.post("/blockdata", (req, res) => {
     const { name, amount, token } = req.body;
     const tokenUser = tokenManager.readToken(token);
     if (tokenUser.error) {
-        return res.json({status:tokenUser.error})
+        return res.json({ status: tokenUser.error })
     }
 
     if (!DUMMY_DB.some(user => user.id == tokenUser.userid)) {
-        return res.json({status:"Sorry, Invalid User"})
+        return res.json({ status: "Sorry, Invalid User" })
     }
 
     if (!name || !amount) {
-        return res.json({status:"Required name, amount"});
+        return res.json({ status: "Required name, amount" });
     }
 
     auto_id++;
-    transactionList.push({
+    io.sockets.transactionList.push({
         id: auto_id,
         userid: tokenUser.userid,
         data: { name, amount }
     });
-    return res.json({status:"done",message:"Your Transaction is added to Queue. Transaction ID is: " + auto_id + "\nView all pending transactions at 'localhost:3000/transactions'\nView BlockChain for completed transactions at 'localhost:3000/blockchain'"});
+
+    return res.json({ status: "done", message: "Your Transaction is added to Queue. Queue ID #" + auto_id + "<br>View all pending transactions <a href='http://localhost:3000/transactions/" + tokenUser.userid + "' target='_blank'>here</a>" });
 
 });
 
@@ -202,15 +207,15 @@ function validateToken(token) {
     if (!DUMMY_DB.some(user => user.id == tokenUser.userid)) {
         return "Error: Sorry, Invalid User"
     }
-    
+
     return "OK";
-    
+
 }
 
 
 function getPendingTransactions(userid, resp) {
     let queueIndex = [];
-    let pendingTransactions = transactionList.filter((transaction, i) => {
+    let pendingTransactions = io.sockets.transactionList.filter((transaction, i) => {
         if (transaction.userid == userid) {
             queueIndex.push(i);
             return true
@@ -218,7 +223,7 @@ function getPendingTransactions(userid, resp) {
         return false
     });
 
-    resp.json({ allUsersTransactions_pending: transactionList.length, currentUser_queueNumber: queueIndex[0], currentUserPending: pendingTransactions });
+    resp.json({ allUsersTransactions_pending: io.sockets.transactionList.length, currentUser_queueNumber: queueIndex[0], currentUserPending: pendingTransactions });
 
 }
 
@@ -256,25 +261,28 @@ function updateLatestBlockChain(block_chain_json) {
 }
 
 async function doTransactions() {
-    console.log("transaction processing for ID: " + transactionList[0].id);
+    console.log("transaction processing for ID: " + io.sockets.transactionList[0].id);
     const new_block = await processBlockChain();
     const { index, user_data, nonce, timestamp, hash, prevHash } = new_block.updated;
     io.sockets.mainBlockChain.addBlock(index, user_data, nonce, timestamp, hash, prevHash, false); // Mining is set to FALSE    
-    transactionList.splice(0, 1);
+    io.sockets.transactionList.splice(0, 1);
     miningActive = false;
     console.log("Block Added");
+    io.sockets.emit("latestBlockChain", {
+        chain: getBlockChain(),
+        remaining: io.sockets.transactionList.length,
+        users: io.sockets.connected_bchain_users.length
+    });
+    if (io.sockets.transactionList.length == 0) console.log("All transactions done");
     return;
 
 }
 
 function processBlockChain() {
     miningActive = true;
-    console.log("BlockChains:");
-    console.log(io.sockets.mainBlockChain);
-
     let lastBlock = io.sockets.mainBlockChain.lastBlock().get();
     return new Promise((resolve, reject) => {
-        const worker = new Worker('./worker.js', { workerData: { blockIndex: lastBlock.index + 1, lastBlockHash: lastBlock.hash, transactionData: transactionList[0].data, difficultyLevel: DIFFICULTY, MASTER_KEY } });
+        const worker = new Worker('./worker.js', { workerData: { blockIndex: lastBlock.index + 1, lastBlockHash: lastBlock.hash, transactionData: io.sockets.transactionList[0].data, difficultyLevel: DIFFICULTY, MASTER_KEY } });
         worker.on('message', resolve);
         worker.on('error', reject);
         worker.on('exit', (code) => {
@@ -291,7 +299,7 @@ const NEW_USER = {
         for (let i = 0; i < 8; i++) {
             id = `${id}${Math.floor(Math.random()*10)}`;
         }
-        id = id.substr(0,4)+"-"+id.substr(4)
+        id = id.substr(0, 4) + "-" + id.substr(4)
         return id;
     },
     getID: function() {
@@ -306,9 +314,12 @@ const NEW_USER = {
 };
 
 let queueInterval = setInterval(function() {
-    if (transactionList.length > 0 && !miningActive) {
-        doTransactions();
+    if(io.sockets.transactionList){
+        if (io.sockets.transactionList.length && !miningActive) {
+            doTransactions();
+        }
     }
+    
 }, 1000);
 
 
--- public/main.js
+++ public/main.js
@@ -1,8 +1,7 @@
-
+const loginBtn = document.querySelector("#signin");
+const registerBtn = document.querySelector("#register");
 const authenticateUser = document.querySelector("#authenticateUser");
 const userTransaction = document.querySelector("#userTransaction");
-const registerBtn = document.querySelector("#register");
-const loginBtn = document.querySelector("#signin");
 const labelText = document.querySelector("label[for=UserEmail]");
 let socket = null;
 
@@ -11,10 +10,10 @@ if (token) {
     fetch("../checktoken/", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ token }) })
         .then(data => data.json())
         .then((res) => {
-            if (res.status == "valid") {                
+            if (res.status == "valid") {
                 document.getElementById("statusmsg").textContent = "You are logged In";
                 authenticateUser.remove();
-                initSocket(res.user);
+                initSocket();
             } else if (res.status == "multiple_login") {
                 alert("Duplicate logIn detected");
             } else {
@@ -25,10 +24,10 @@ if (token) {
 }
 
 registerBtn.addEventListener("click", function(e) {
-    labelText.textContent="User Email:";
+    labelText.textContent = "User Email:";
 });
 loginBtn.addEventListener("click", function(e) {
-    labelText.textContent="User ID:";
+    labelText.textContent = "User ID:";
 })
 
 authenticateUser.addEventListener("submit", function(e) {
@@ -43,7 +42,7 @@ authenticateUser.addEventListener("submit", function(e) {
 
     }
     if (email.trim() !== "" && password.trim() !== "") {
-        submitForm(payload,restUrl)
+        authSubmitForm(payload, restUrl)
     } else {
         alert("Field(s) are empty")
     }
@@ -52,55 +51,80 @@ authenticateUser.addEventListener("submit", function(e) {
 userTransaction.addEventListener("submit", function(e) {
     e.preventDefault();
     const payload = {
-        name:userTransaction.personName,
-        amount:userTransaction.amount,
-        token:sessionStorage.getItem("token")
+        name: userTransaction.personName.value,
+        amount: userTransaction.amount.value,
+        token: sessionStorage.getItem("token")
     };
 
-    
-     fetch("../blockdata", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(payload) })
-    .then(data => data.json())
-    .then((res) => {
-        if(res.status=="done"){
-            document.getElementById("statusmsg").textContent = res.message;
-        }else{
-            alert(res.status)
-        }
-    });
-    
-
+    fetch("../blockdata", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(payload) })
+        .then(data => data.json())
+        .then((res) => {
+            if (res.status == "done") {
+                document.getElementById("statusmsg").innerHTML = res.message;
+                userTransaction.reset();
+            } else {
+                alert(res.status)
+            }
+        });
 
 });
 
-function submitForm(payload,restUrl){
-    fetch(restUrl, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(payload) })
-    .then(data => data.json())
-    .then((res) => {
-        if (res.status == "done") {            
-            sessionStorage.setItem("token", res.token);
-            let txtMsg = "";
-            if (restUrl.includes("register")) {
-                txtMsg = `Generated User ID is: ${res.userID}.  Token saved successfuly!`
-            } else {
-                txtMsg = "You have successfuly logged In"
-            }
-            authenticateUser.remove();
-            userTransaction.classList.remove("hide");
-            document.getElementById("statusmsg").textContent = txtMsg;
-            initSocket(res.userID);
-        } else {
-            alert(res.status)
-        }
 
-    });
+function authSubmitForm(payload, restUrl) {
+    fetch(restUrl, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(payload) })
+        .then(data => data.json())
+        .then((res) => {
+            if (res.status == "done") {
+                sessionStorage.setItem("token", res.token);
+                let txtMsg = "";
+                if (restUrl.includes("register")) {
+                    txtMsg = `Generated User ID is: ${res.userID}.  Token saved successfuly!`
+                } else {
+                    txtMsg = "You have successfuly logged In"
+                }
+                authenticateUser.remove();
+                document.getElementById("statusmsg").textContent = txtMsg;
+                initSocket();
+            } else {
+                alert(res.status)
+            }
+
+        });
+}
+
+function insertTransactionDOM() {
+    const formDiv = document.createElement("div");
+    const formElts = `<p>
+            <label for="personName">Name:</label><input type="text" id="personName">
+        </p>
+        <p>
+            <label for="amount">Amount:</label><input type="text" id="amount">
+        </p>
+        <br><br>
+        <input type="submit" value="Send Amount">`;
+    formDiv.innerHTML = formElts;
+    userTransaction.appendChild(formDiv)
+
 }
 
 
-function initSocket(userid) {
+function initSocket() {
     socket = io();
-    socket.emit('addToUserList', userid);
     socket.on('ConnectedUsers', (usersNum) => {
-        document.getElementById("total-users").textContent = usersNum;
+        document.getElementById("total-users").textContent = "Users: " + usersNum;
+        insertTransactionDOM();
     });
+    socket.on('latestBlockChain', (bc) => {        
+        const blockchainString = JSON.stringify(bc.chain);
+        const downloadLink = document.getElementById("bc_download");
+        const bcFile = new Blob([blockchainString], { type: "text/plain" });
+        downloadLink.href = URL.createObjectURL(bcFile);
+        downloadLink.download = "blockchain.text";
+        document.getElementById("pendings").textContent = " / Pending Transactions: " + bc.remaining;
+        document.getElementById("total-users").textContent = "Users: " + bc.users;
+        alert("Received New BlockChain. Use the Download link")
+
+    });
+    socket.emit('addToUserList', { token: sessionStorage.getItem("token") });
 
 }
