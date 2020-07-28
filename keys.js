ed587312ba0553cb28a88843d575a9bebd1fdc18
deepak <deepakb@protonmail.com>
2020-07-28 20:25:37 +0530

    clear up new repo

--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	masterKey: "snWiNeznv53RdvURFFdiLBqT0fhmx1nBTi3L0hcLUR6x00SVl6",
-	tokenAccess: "fjsdh87538idfsJHD89JHDklSJHBFkfn"
-}
--- worker.js
+++
@@ -1,8 +0,0 @@
-const { workerData, parentPort } = require('worker_threads');
-const BlockChain = require("./modules/blockchain");
-
-console.log("Worker thread. Mining initiated....");
-const newBlockChain = BlockChain(workerData.difficultyLevel, workerData.MASTER_KEY);
-newBlockChain.addBlock(workerData.blockIndex, workerData.transactionData, null, null, null, workerData.lastBlockHash);
-
-parentPort.postMessage({ updated: newBlockChain.lastBlock().get() })
